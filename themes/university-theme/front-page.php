9d06b12f2f3db2aed234ee02b504c225139ea9a1
Alexandru Voinescu <marius.a.voinescu@gmail.com>
2019-02-08 09:11:50 +0200

    2019.02.08
    -Finished the custom events by adding the correct archive and page
    -added the content export for easy work across multiple dev env without the needed of adding the content again

--- themes/university-theme/front-page.php
+++ themes/university-theme/front-page.php
@@ -38,8 +38,8 @@
 							<span class="event-summary__day">25</span>
 						</a>
 						<div class="event-summary__content">
-							<h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-							<p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+							<h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
+							<p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
 						</div>
 					</div>
 				<?php } wp_reset_postdata();
